7243de1576369384f44a0dfebd8ca0695dbd9e2e
JeffM2501 <JeffM2501@4c235640-23d2-11df-bd34-830ff933dde4>
2010-02-27 19:09:11 +0000

    move P2501 from gplib to OC

---
+++ trunk/P2501/Services/Auth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AuthServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AuthHost(args[0]).Run();
+        }
+    }
+}
