444910d6c420dc565ca8f1360180d997eb8555c5
jeffm2501 <jeffm2501@4c235640-23d2-11df-bd34-830ff933dde4>
2010-03-11 07:00:53 +0000

    Add GUI test to test/create new GUI elements.

--- P2501/Components/GUIObjects/Components.cs
+++ P2501/Components/GUIObjects/Components.cs
@@ -5,14 +5,78 @@ using System.Text;
 
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using Drawables.Textures;
+using Utilities.Paths;
 
 namespace GUIObjects
 {
-    public class Frame: GrapicalGuiObject
+    public class Frame: GUIObject
     {
+        string backgroundTexture = string.Empty;
+        Texture BGTexture = null;
+
+        public Frame()
+        {
+            Name = "Frame";
+        }
+
+        protected override void ReadExtraDefInfo(ElementDefinition def)
+        {
+            base.ReadExtraDefInfo(def);
+
+            backgroundTexture = def.GetOptionValue("BackgroundTexture");
+            if (backgroundTexture != string.Empty)
+                BGTexture = TextureSystem.system.GetTexture(ResourceManager.FindFile(backgroundTexture));
+        }
+
+        protected override void WriteExtraDefInfo(ElementDefinition def)
+        {
+            base.WriteExtraDefInfo(def);
+            def.SetOptionValue("BackgroundTexture", backgroundTexture);
+        }
+
         protected override void Render(double time)
         {
+            GL.Color4(BackgroundColor);
+            if (BGTexture != null)
+                BGTexture.Draw(size.Width, size.Height);
+            else
+            {
+                GL.Disable(EnableCap.Texture2D);
+                GL.Begin(BeginMode.Quads);
 
+                GL.Vertex2(0, 0);
+                GL.Vertex2(size.Width, 0);
+                GL.Vertex2(size.Width, size.Height);
+                GL.Vertex2(0, size.Height);
+
+                GL.End();
+            }
+        }
+    }
+
+    public class GroupBox : GUIObject
+    {
+        public GroupBox()
+        {
+            Name = "GroupBox";
+        }
+
+        protected override void Render(double time)
+        {
+            base.Render(time);
+
+            GL.Color4(ForegroundColor);
+
+            GL.Disable(EnableCap.Texture2D);
+            GL.Begin(BeginMode.LineLoop);
+
+            GL.Vertex2(0, 0);
+            GL.Vertex2(size.Width, 0);
+            GL.Vertex2(size.Width, size.Height);
+            GL.Vertex2(0, size.Height);
+
+            GL.End();
         }
     }
 }
