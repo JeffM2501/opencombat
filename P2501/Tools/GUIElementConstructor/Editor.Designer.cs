dac5cb99207b13648d7d67259439e94f174cbea2
JeffM2501 <JeffM2501@4c235640-23d2-11df-bd34-830ff933dde4>
2010-03-13 02:36:28 +0000

    Tool to define GUI elements

---
+++ P2501/Tools/GUIElementConstructor/Editor.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace GUIElementConstructor
+{
+    partial class Editor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.GLView = new OpenTK.GLControl();
+            this.ComponentList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.GLView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.ComponentList);
+            this.splitContainer1.Size = new System.Drawing.Size(1032, 484);
+            this.splitContainer1.SplitterDistance = 651;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // GLView
+            // 
+            this.GLView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.GLView.BackColor = System.Drawing.Color.Black;
+            this.GLView.Location = new System.Drawing.Point(3, 3);
+            this.GLView.Name = "GLView";
+            this.GLView.Size = new System.Drawing.Size(645, 478);
+            this.GLView.TabIndex = 0;
+            this.GLView.VSync = false;
+            this.GLView.Load += new System.EventHandler(this.GLView_Load);
+            this.GLView.Paint += new System.Windows.Forms.PaintEventHandler(this.GLView_Paint);
+            this.GLView.Resize += new System.EventHandler(this.GLView_Resize);
+            // 
+            // ComponentList
+            // 
+            this.ComponentList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ComponentList.FormattingEnabled = true;
+            this.ComponentList.Location = new System.Drawing.Point(214, 24);
+            this.ComponentList.Name = "ComponentList";
+            this.ComponentList.Size = new System.Drawing.Size(151, 121);
+            this.ComponentList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Components";
+            // 
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Interval = 10;
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 150);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(151, 322);
+            this.treeView1.TabIndex = 2;
+            // 
+            // Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 484);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Editor";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private OpenTK.GLControl GLView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ComponentList;
+        private System.Windows.Forms.Timer UpdateTimer;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ P2501/Tools/GUIElementConstructor/Editor.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+using Drawables.Cameras;
+using Drawables.Textures;
+using Utilities.Paths;
+
+using GUIObjects;
+
+namespace GUIElementConstructor
+{
+    public partial class Editor : Form
+    {
+        Camera camera;
+
+        Stopwatch stopwatch = new Stopwatch();
+
+        Texture cautionTexture;
+        Texture grass;
+
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void GLView_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(System.Drawing.Color.LightSkyBlue);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.Enable(EnableCap.CullFace);
+            GL.CullFace(CullFaceMode.Back);
+            GL.FrontFace(FrontFaceDirection.Ccw);
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.PolygonMode(MaterialFace.Front, PolygonMode.Fill);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.Enable(EnableCap.ColorMaterial);
+            GL.Enable(EnableCap.LineSmooth);
+            GL.LightModel(LightModelParameter.LightModelColorControl, 1);
+
+            // setup light 0
+            Vector4 lightInfo = new Vector4(0.5f, 0.5f, 0.5f, 1.0f);
+            GL.Light(LightName.Light0, LightParameter.Ambient, lightInfo);
+
+            lightInfo = new Vector4(0.7f, 0.7f, 0.7f, 1.0f);
+            GL.Light(LightName.Light0, LightParameter.Diffuse, lightInfo);
+            GL.Light(LightName.Light0, LightParameter.Specular, lightInfo);
+
+            camera = new Camera();
+            camera.set(new Vector3(0, -8, 5), -25, 90);
+
+            ResourceManager.AddPath("../../../../../p2501_data");
+            ResourceManager.AddPath("../../../../p2501_data");
+            ResourceManager.AddPath("../../../p2501_data");
+            ResourceManager.AddPath("../../p2501_data");
+            ResourceManager.AddPath("../p2501_data");
+            ResourceManager.AddPath("./p2501_data");
+
+            GUIObjectManager.AddDefaultComponents();
+
+            cautionTexture = TextureSystem.system.GetTexture(ResourceManager.FindFile("Textures/BZ/caution.png"));
+            grass = TextureSystem.system.GetTexture(ResourceManager.FindFile("Textures/BZ/grass.png"));
+
+            stopwatch.Start();
+            UpdateTimer.Start();
+
+            GLView_Resize(this,EventArgs.Empty);
+
+            foreach (KeyValuePair<string,Type> component in GUIObjectManager.Components)
+                ComponentList.Items.Add(component.Key);
+        }
+
+        protected void XZQuad( float width, float height)
+        {
+            GL.Begin(BeginMode.Quads);
+
+            GL.Normal3(0, 1, 0);
+            GL.TexCoord2(0, 1);
+            GL.Vertex3(0, 0, 0);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex3(width, 0, 0);
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex3(width, 0, height);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex3(0, 0, height);
+
+            GL.End();
+        }
+
+        protected void XZQuadBackwards(float width, float height)
+        {
+            GL.Begin(BeginMode.Quads);
+
+            GL.Normal3(0, -1, 0);
+            GL.TexCoord2(0, 0);
+            GL.Vertex3(0, 0, height);
+           
+            GL.TexCoord2(1, 0);
+            GL.Vertex3(width, 0, height);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex3(width, 0, 0);
+            
+            GL.TexCoord2(0, 1);
+            GL.Vertex3(0, 0, 0);
+
+            GL.End();
+        }
+
+        private void GLView_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Disable(EnableCap.Texture2D);
+            GL.Disable(EnableCap.Lighting);
+            camera.SetPersective();
+            camera.Execute();
+
+            GL.PushMatrix();
+            GL.Enable(EnableCap.Lighting);
+            GL.Enable(EnableCap.Light0);
+            GL.Light(LightName.Light0, LightParameter.Position, new Vector4(0, -15, 10, 1.0f));
+
+            float angle = stopwatch.ElapsedMilliseconds / 1000f * 45f;
+            if (cautionTexture != null)
+                cautionTexture.Bind();
+
+            GL.Rotate(angle, 0, 0, 1);
+            GL.Color4(Color.White);
+            for (int i = 0; i < 4; i++)
+            {
+                GL.Rotate(90, 0, 0, 1);
+                XZQuad(2, 2);
+                XZQuadBackwards(2, 2);
+            }
+            GL.PopMatrix();
+
+            GL.PushMatrix();
+
+            GL.Translate(-15, -15, 0);
+            if (grass != null)
+                grass.Draw(30, 30, 128);
+
+            GL.PopMatrix();
+
+            GL.Disable(EnableCap.Texture2D);
+            GL.Disable(EnableCap.Lighting);
+            camera.SetOrthographic();
+
+            GL.Translate(0, 0, -1);
+
+            DrawUI();
+
+            GLView.SwapBuffers();
+        }
+
+        private void DrawUI ()
+        {
+            foreach(GUIObject element in GUIObjectManager.Elements)
+                element.Draw(stopwatch.ElapsedMilliseconds / 1000.0);
+        }
+
+        private void GLView_Resize(object sender, EventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, GLView.Size.Width, GLView.Size.Height);
+            GUIObjectManager.Resize(GLView.Size.Width, GLView.Size.Height);
+            if (camera != null)
+                camera.Resize(GLView.Size.Width, GLView.Size.Height);
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            GLView.Invalidate(true);
+        }
+    }
+}
