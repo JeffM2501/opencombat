2443caf260aa93cfd4ac29af4d5dada7e38d7c2c
JeffM2501@gmail.com <JeffM2501@gmail.com@4c235640-23d2-11df-bd34-830ff933dde4>
2010-08-11 16:45:32 +0000

    start HUD

---
+++ P2501/Apps/Client/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Simulation;
+
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace P2501Client
+{
+    public class HUDRenderer
+    {
+        public Player ThePlayer = null;
+
+        protected GameWindow Win;
+
+        public HUDRenderer ( GameWindow window )
+        {
+            Win = window;
+            Win.Resize += new EventHandler<EventArgs>(Win_Resize);
+        }
+
+        void Win_Resize(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update ( double time )
+        {
+
+        }
+    }
+}
