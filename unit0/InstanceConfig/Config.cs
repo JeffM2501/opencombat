265d02d897bdd37cd039cee0a21efee7f6146a10
jeffm2501 <jeffm2501@4c235640-23d2-11df-bd34-830ff933dde4>
2012-04-02 22:20:34 +0000

    add basic configuration for game instances.

---
+++ unit0/InstanceConfig/Config.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace InstanceConfig
+{
+    public class Config
+    {
+        public static Config Empty = new Config();
+
+        public string MapFilePath = string.Empty;
+        public string GameMode = string.Empty;
+
+        public int Port = -1;
+        public string ManagerAddress = string.Empty;
+
+        public string Name = string.Empty;
+
+        public static Config Read(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            if (!file.Exists)
+                return Empty;
+
+            FileStream fs = file.OpenRead();
+            XmlSerializer xml = new XmlSerializer(typeof(Config));
+            Config c = (Config)xml.Deserialize(fs);
+            fs.Close();
+            return c;
+        }
+
+        public bool Write(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            if (!file.Exists)
+                file.Delete();
+
+            try
+            {
+                FileStream fs = file.OpenWrite();
+                XmlSerializer xml = new XmlSerializer(typeof(Config));
+                xml.Serialize(fs,this);
+                fs.Close();
+            }
+            catch (System.Exception /*ex*/)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
