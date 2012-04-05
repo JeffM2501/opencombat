f210f61a0e52c932608ed35a374f5d3dcea2704e
JeffM2501 <JeffM2501@4c235640-23d2-11df-bd34-830ff933dde4>
2012-04-05 04:59:02 +0000

    use reflection to pack and unpack any network message.

--- unit0/InstanceConfig/InstanceMessages.cs
+++ unit0/InstanceConfig/InstanceMessages.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+
+using Lidgren.Network;
 
 namespace InstanceConfig
 {
@@ -10,9 +13,65 @@ namespace InstanceConfig
         public enum MessageCode
         {
             Null = 0,
+            Connect,
+        }
 
+        public MessageCode Code = MessageCode.Null;
+
+        public static Dictionary<MessageCode, Type> MessageClasses = new Dictionary<MessageCode, Type>();
+
+        public delegate void MessageReceived ( MessageCode code, InstanceMessage messageData );
+
+        public static Dictionary<Type, List<MessageReceived>> ReceivedCallbacks = new Dictionary<Type, List<MessageReceived>>();
+
+        public static void Process(NetIncomingMessage msg)
+        {
+            InstanceMessage messageData = Unpack(msg);
+            if (messageData != null && ReceivedCallbacks.ContainsKey(messageData.GetType()))
+            {
+                foreach (MessageReceived cb in ReceivedCallbacks[messageData.GetType()])
+                    cb(messageData.Code, messageData);
+            }
+        }
+
+        public static void Register()
+        {
+            foreach (Type t in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (t.IsSubclassOf(typeof(InstanceMessage)))
+                {
+                    MessageCode code = ((InstanceMessage)Activator.CreateInstance(t)).Code;
+                    if (!MessageClasses.ContainsKey(code))
+                        MessageClasses.Add(code, t);
+                }
+            }
+        }
+
+        public NetOutgoingMessage Pack(NetOutgoingMessage msg)
+        {
+            if (MessageClasses.Count ==- 0)
+                Register();
+
+            msg.Write((byte)Code);
+            msg.WriteAllFields(this);
+            return msg;
+        }
+
+        public static InstanceMessage Unpack(NetIncomingMessage msg)
+        {
+            byte c = msg.ReadByte();
+            MessageCode code = (MessageCode)Enum.ToObject(typeof(MessageCode),c);
+
+            if (MessageClasses.ContainsKey(code))
+            {
+                InstanceMessage dataClass = (InstanceMessage)Activator.CreateInstance(MessageClasses[code]);
+                msg.ReadAllFields(dataClass);
+                return dataClass;
+            }
+            return null;
         }
     }
+
     public class InstanceConnect
     {
     }
