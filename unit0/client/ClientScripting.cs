180680b59fb87584bed2c372ab64c3f307e4728d
jeffm2501 <jeffm2501@4c235640-23d2-11df-bd34-830ff933dde4>
2012-04-09 23:10:32 +0000

    remove plugins and replace with scripting. connect to server from client with threaded message processor.

---
+++ unit0/client/ClientScripting.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+
+using Game;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace Client
+{
+    public class ClientScripting
+    {
+        public static ClientScripting Script = new ClientScripting();
+
+        ScriptEngine Engine = null;
+
+        protected Dictionary<string, ScriptSource> CachedScripts = new Dictionary<string, ScriptSource>();
+
+        public string ScriptPackName = string.Empty;
+
+        GameState State = null;
+
+        public void Init(string scriptPath, GameState state)
+        {
+            // TODO, put an app domain here that dosn't give access to the disk
+            Engine = Python.CreateEngine();
+
+            foreach (FileInfo script in new DirectoryInfo(scriptPath).GetFiles("*.py"))
+            {
+                ScriptSource source = Engine.CreateScriptSourceFromFile(script.FullName, Encoding.ASCII, SourceCodeKind.File);
+                CachedScripts.Add(Path.GetFileNameWithoutExtension(script.Name), source);
+            }
+
+            ScriptPackName = Path.GetDirectoryName(scriptPath);
+
+            State = state;
+        }
+
+        protected ScriptScope GetScope(ScriptSource source)
+        {
+            ScriptScope scope = Engine.CreateScope();
+
+            scope.SetVariable("World", State.GameWorld);
+            scope.SetVariable("State", State);
+
+            source.Execute(scope);
+            return scope;
+        }
+
+        protected ScriptScope GetScope(string name)
+        {
+            if (!CachedScripts.ContainsKey(name))
+                return null;
+
+            return GetScope(CachedScripts[name]);
+        }
+
+        public bool InitGameScript()
+        {
+            ScriptScope scope = GetScope("Game");
+            if (scope == null)
+                return false;
+
+            dynamic func = scope.GetVariable("InitGame");
+            return func();
+        }
+    }
+}
