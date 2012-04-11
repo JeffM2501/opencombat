dd4b15f6f52179857c98ffaae1f1f4eb867e7716
jeffm2501 <jeffm2501@4c235640-23d2-11df-bd34-830ff933dde4>
2012-04-11 23:26:09 +0000

    update code to use new texture smoothing option. have hud draw a connection screen with a status message as we connect to server.

--- trunk/unit0/client/View/View.Hud.cs
+++ trunk/unit0/client/View/View.Hud.cs
@@ -22,21 +22,27 @@ namespace Client
         public View TheView = null;
 
         Font WaitFont = new Font(FontFamily.GenericSansSerif, 72);
+        Font WaitInfoFont = new Font(FontFamily.GenericSansSerif, 14);
         public TextPrinter TextPrinter = new TextPrinter(TextQuality.High);
 
         protected Stopwatch Clock = new Stopwatch();
 
-        Texture Spinner = null;
+        Texture[] Spinners = null;
+
+        public string StateMessage = string.Empty;
+
+        protected string WaitHeader = "Processing";
 
         public HUD(View view)
         {
             TheView = view;
-            
         }
 
         public void Init()
         {
-            Spinner = Texture.Get(Locations.FindDataFile("ui/wait_ring.png"));
+            Spinners = new Texture[2];
+            Spinners[0] = Texture.Get(Locations.FindDataFile("ui/outer_spinner.png"), Texture.SmoothType.SmoothMip, false);
+            Spinners[1] = Texture.Get(Locations.FindDataFile("ui/inner_spinner.png"), Texture.SmoothType.SmoothMip, false);
             Clock.Start();
         }
 
@@ -68,9 +74,117 @@ namespace Client
 
         float lastWaitAngle = 0;
 
+        protected class Swoosh
+        {
+            public Vector2 position = Vector2.Zero;
+            public float speed = 1;
+            public Vector2 size = Vector2.Zero;
+            public float alpha = 1;
+
+            public static int BaseSwooshDensity = 1;
+            public static int SwooshDensityVariant = 5;
+            public static int SwooshDensity = 0;
+
+            public static int SwooshBaseHeight = 10;
+            public static int SwooshHeightVariant = 50;
+
+            public static int SwooshBaseLenght = 200;
+            public static int SwooshLenghtVariant = 1000;
+
+            public static float SwooshBaseAlpha = 0.001f;
+            public static float SwooshAlphaVariant = 0.15f;
+
+            public static float SwooshBaseSpeed = 200.0f;
+            public static float SwooshSpeedVariant = 2050.0f;
+
+            public static int SwooshCenterSpread = 200;
+
+            public static int SwooshAngleOffset = 50;
+
+            public Swoosh()
+            {
+                size = NewSize();
+                alpha = NewAlpha();
+                speed = NewSpeed();
+            }
+
+            public void Draw( float z)
+            {
+                GL.PushMatrix();
+                GL.Color4(1,1,1,alpha);
+                GL.Translate(position.X,position.Y,z);
+                GL.Normal3(Vector3.UnitZ);
+
+                GL.Begin(BeginMode.Quads);
+                GL.Vertex2(SwooshAngleOffset,0);
+                GL.Vertex2(size.X,0);
+                GL.Vertex2(size.X-SwooshAngleOffset,size.Y);
+                GL.Vertex2(0,size.Y);
+                GL.End();
+                GL.PopMatrix();
+            }
+
+            public static Vector2 NewSize()
+            {
+                Random rand = new Random();
+
+                return new Vector2(SwooshBaseLenght + rand.Next(SwooshLenghtVariant), SwooshBaseHeight + rand.Next(SwooshHeightVariant));
+            }
+
+            public static float NewAlpha()
+            {
+                Random rand = new Random();
+
+                return (float)(SwooshBaseAlpha + (rand.NextDouble()*SwooshAlphaVariant));
+            }
+
+            public static float NewSpeed()
+            {
+                Random rand = new Random();
+
+                return (float)(SwooshBaseSpeed + (rand.NextDouble()*SwooshSpeedVariant));
+            }
+
+            public static void NewDensity()
+            {
+                Swoosh.SwooshDensity = Swoosh.BaseSwooshDensity + new Random().Next(Swoosh.SwooshDensityVariant);
+            }
+        }
+
+        protected List<Swoosh>[] Swooshes = new List<Swoosh>[2]{new List<Swoosh>(),new List<Swoosh>()};
+
+        public float NewSwooshY()
+        {
+            Random rand = new Random();
+
+            int offset = rand.Next(Swoosh.SwooshCenterSpread * 2) - Swoosh.SwooshCenterSpread;
+
+            return (TheView.Window.Height/3.5f) + offset;
+        }
+       
+        protected void InitSwoshes()
+        {
+            if (Swooshes[0].Count + Swooshes[1].Count > 0)
+                return;
+
+            Random rand = new Random();
+
+            Swoosh.NewDensity();
+            for (int i = 0; i < Swoosh.SwooshDensity; i++)
+            {
+                Swoosh swoosh = new Swoosh();
+                swoosh.position = new Vector2((float)rand.NextDouble() * TheView.Window.Width,NewSwooshY());
+                Swooshes[rand.Next(1)].Add(swoosh);
+            }
+        }
+
         protected void DrawWaitScreen()
         {
+            GL.Disable(EnableCap.DepthTest);
+            GL.DepthMask(false);
             GL.Disable(EnableCap.Texture2D);
+            foreach (Swoosh swoosh in Swooshes[0])
+                swoosh.Draw(-2);
 
             double DotTime = 1;
 
@@ -88,12 +202,15 @@ namespace Client
                 d += ".";
 
             TextPrinter.Begin();
-            SizeF s = TextPrinter.Measure("Connecting", WaitFont).BoundingBox.Size;
+            SizeF s = TextPrinter.Measure(WaitHeader, WaitFont).BoundingBox.Size;
 
             float buffer = TheView.Window.Width - s.Width;
             RectangleF r = new RectangleF(buffer / 2, (TheView.Window.Height / 2) - (s.Height / 2), TheView.Window.Width - (buffer / 2), s.Height);
 
-            TextPrinter.Print("Connecting" + d, WaitFont, Color.White,r, TextPrinterOptions.Default,TextAlignment.Near);
+            TextPrinter.Print(WaitHeader + d, WaitFont, Color.White, r, TextPrinterOptions.Default, TextAlignment.Near);
+
+            r = new RectangleF(buffer/2 + 10, (TheView.Window.Height / 2) + (s.Height/2) - 10, TheView.Window.Width - (buffer / 2), 40);
+            TextPrinter.Print(StateMessage, WaitInfoFont, Color.White, r, TextPrinterOptions.Default, TextAlignment.Near);
             TextPrinter.End();
 
             GL.Enable(EnableCap.Texture2D);
@@ -101,13 +218,55 @@ namespace Client
             float spinspeed = 120;
             lastWaitAngle += spinspeed * (float)Delta;
 
-            Spinner.Bind();
             GL.PushMatrix();
             GL.Translate(buffer / 4, (TheView.Window.Height / 2), -1);
+
+            GL.PushMatrix();
             GL.Rotate(lastWaitAngle, Vector3.UnitZ);
 
-            GeoUtils.Quad(Vector2.Zero, new Vector2(Spinner.Width, Spinner.Height), 0, Color.White);
+            float scale = 0.25f;
+            Spinners[0].Draw(scale);
             GL.PopMatrix();
+
+            GL.Translate(0,0, 0.01f);
+            GL.Rotate(-lastWaitAngle * 1.034f, Vector3.UnitZ);
+            Spinners[1].Draw(scale);
+            GL.PopMatrix();
+
+            GL.Disable(EnableCap.Texture2D);
+            foreach (Swoosh swoosh in Swooshes[1])
+                swoosh.Draw(-0.5f);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthMask(true);
+
+            // update all the swooshes
+            foreach (List<Swoosh> swooshes in Swooshes)
+            {
+                List<Swoosh> toKill = new List<Swoosh>();
+                foreach (Swoosh swoosh in swooshes)
+                {
+                    swoosh.position.X -= swoosh.speed * (float)Delta;
+                    if ( swoosh.position.X + swoosh.size.X < 0)
+                        toKill.Add(swoosh);
+                }
+
+                 foreach (Swoosh swoosh in toKill)
+                     swooshes.Remove(swoosh);
+            }
+
+            Swoosh.NewDensity();
+            if (Swooshes[0].Count + Swooshes[1].Count < Swoosh.SwooshDensity)
+            {
+                Random rand = new Random();
+                int newSwooshes = Swoosh.SwooshDensity - (Swooshes[0].Count + Swooshes[1].Count);
+                for (int i = 0; i < newSwooshes; i++)
+                {
+                    Swoosh swoosh = new Swoosh();
+                    swoosh.position = new Vector2(TheView.Window.Width + 2,NewSwooshY());
+                    Swooshes[rand.Next(1)].Add(swoosh);
+                }
+            }
         }
 
         public void StatusChange ( View.ViewStatus status)
@@ -117,7 +276,17 @@ namespace Client
             if (status == View.ViewStatus.Playing)
                 GL.ClearColor(Color.LightSkyBlue);
             else
+            {
                 GL.ClearColor(Color.Black);
+                InitSwoshes();
+
+                if (status == View.ViewStatus.Connecting)
+                    WaitHeader = "Connecting";
+                else if (status == View.ViewStatus.Errored)
+                    WaitHeader = "Error";
+                else
+                    WaitHeader = "Loading";
+            }
         }
     }
 }
