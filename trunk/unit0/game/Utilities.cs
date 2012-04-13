0c1168141b7b2e9b724feec8015490945276b2b0
JeffM2501 <JeffM2501@4c235640-23d2-11df-bd34-830ff933dde4>
2012-04-13 05:02:26 +0000

    Common utils, like computing an MD5

---
+++ trunk/unit0/game/Utilities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Game
+{
+    public class Utilities
+    {
+        public static string GetMD5Hash( byte[] buffer )
+        {
+            MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(buffer);
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < buffer.Length; i++)
+                sBuilder.Append(buffer[i].ToString("x2"));
+
+            return sBuilder.ToString();
+        }
+
+        public static string GetMD5Hash(string buffer)
+        {
+            return GetMD5Hash(Encoding.UTF8.GetBytes(buffer));
+        }
+
+        public static string GetMD5Hash(FileInfo file)
+        {
+            FileStream fs = file.OpenRead();
+            byte[] buffer = new byte[file.Length];
+            fs.Read(buffer, buffer.Length, 1);
+            fs.Close();
+            return GetMD5Hash(buffer);
+        }
+    }
+}
