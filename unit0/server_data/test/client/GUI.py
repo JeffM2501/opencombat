62f9cc17c5dcd987a03398e6ff30be03c8c7f2c1
JeffM2501 <JeffM2501@4c235640-23d2-11df-bd34-830ff933dde4>
2012-04-22 19:19:58 +0000

    add placeholders for the rest of the test GUI. add FPS counter using GUI system to replace hardcoded FPS.

--- unit0/server_data/test/client/GUI.py
+++ unit0/server_data/test/client/GUI.py
@@ -1,11 +1,9 @@
-
-
 def LoadHud (HUD):
     chatFrame = HUD.NewElement("chatbox","chatbox");
     chatFrame.alignment = HUD.AlignRightBottom;
     chatFrame.origin = HUD.AlignRightBottom;
     chatFrame.SetPosition(0,0);
-    chatFrame.SetSize(500,250);
+    chatFrame.SetSize(500,128);
     chatFrame.enabled = True;
     
     chatList = chatFrame.NewChild("chatWindow","chatWindow");
@@ -13,8 +11,50 @@ def LoadHud (HUD):
     chatList.alignment = HUD.AlignLeftBottom;
     chatList.origin = HUD.AlignLeftBottom;
     chatList.SetPosition(15,35);
-    chatList.SetSize(460,200);
+    chatList.SetSize(460,80);
     chatList.enabled = True;
 
     HUD.LoadElement(chatFrame);
+
+    radarFrame = HUD.NewElement("radar","image");
+    radarFrame.alignment = HUD.AlignLeftBottom;
+    radarFrame.origin = HUD.AlignLeftBottom;
+    radarFrame.SetPosition(0,0);
+    radarFrame.SetSize(256,256);
+    radarFrame.enabled = True;
+    radarFrame.text = "ui/radar.png";
+    HUD.LoadElement(radarFrame);
+    
+    playerFrame = HUD.NewElement("stats","image");
+    playerFrame.alignment = HUD.AlignRightTop;
+    playerFrame.origin = HUD.AlignRightTop;
+    playerFrame.SetPosition(0,0);
+    playerFrame.SetSize(256,128);
+    playerFrame.enabled = True;
+    playerFrame.text = "ui/playerInfo.png";
+    HUD.LoadElement(playerFrame);
+    
+    infoFrame = HUD.NewElement("info","image");
+    infoFrame.alignment = HUD.AlignLeftTop;
+    infoFrame.origin = HUD.AlignLeftTop;
+    infoFrame.SetPosition(0,0);
+    infoFrame.SetSize(256,64);
+    infoFrame.enabled = True;
+    infoFrame.text = "ui/short_upper_left.png";
+    
+    fpsItem = infoFrame.NewChild("fps","statsFontLabel");
+    fpsItem.alignment = HUD.AlignLeftTop;
+    fpsItem.origin = HUD.AlignLeftTop;
+    fpsItem.SetPosition(5,-5);
+    fpsItem.text = "FPS:";
+    fpsItem.enabled = True;
+
+    fpsItem = infoFrame.NewChild("fps","statsFontLabel");
+    fpsItem.updateFunction = "FPS";
+    fpsItem.alignment = HUD.AlignLeftTop;
+    fpsItem.origin = HUD.AlignLeftTop;
+    fpsItem.SetPosition(65,-5);
+    fpsItem.enabled = True;
+    
+    HUD.LoadElement(infoFrame);
     return True;
