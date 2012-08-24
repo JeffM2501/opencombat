843efe42543546f181577e9ca2374261c0822ea1
JeffM2501@gmail.com <JeffM2501@gmail.com@4c235640-23d2-11df-bd34-830ff933dde4>
2012-08-24 03:55:46 +0000

    stub out files for controller and views

---
+++ unit0/PlanEdit/3dView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PlanEdit
+{
+	public partial class _3dView : Form
+	{
+		public _3dView()
+		{
+			InitializeComponent();
+		}
+
+		private void glControl1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void glControl1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void glControl1_Resize(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
--- unit0/PlanEdit/EditView.cs
+++ unit0/PlanEdit/EditView.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+
+using GridWorld;
+
 namespace PlanEdit
 {
-    public partial class Form1 : Form
+    public partial class EditView : Form
     {
-        public Form1()
+        public EditView()
         {
             InitializeComponent();
         }
