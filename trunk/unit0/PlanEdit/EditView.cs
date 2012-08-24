1498d85f0079416ca164ef398e3af38a60ffdd37
JeffM2501@gmail.com <JeffM2501@gmail.com@4c235640-23d2-11df-bd34-830ff933dde4>
2012-08-24 04:39:26 +0000

    load prefs and pick data dir so views and editors can know stuff.

--- trunk/unit0/PlanEdit/EditView.cs
+++ trunk/unit0/PlanEdit/EditView.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 using GridWorld;
 
@@ -14,6 +16,9 @@ namespace PlanEdit
 {
     public partial class EditView : Form
     {
+		ClusterEditor Editor = null;
+		PlanRenderer PlanView = null;
+
         public EditView()
         {
             InitializeComponent();
@@ -23,5 +28,49 @@ namespace PlanEdit
         {
 
         }
+
+		public void SavePrefs(Prefs pref)
+		{
+			string prefsFile = Path.Combine(Application.CommonAppDataPath, "Prefs.xml");
+			if (File.Exists(prefsFile))
+				File.Delete(prefsFile);
+
+			XmlSerializer xml = new XmlSerializer(typeof(Prefs));
+			FileStream fs = new FileStream(prefsFile, FileMode.OpenOrCreate, FileAccess.Write);
+			xml.Serialize(fs, pref);
+			fs.Close();
+		}
+
+		private void EditView_Load(object sender, EventArgs e)
+		{
+			Prefs prefs = new Prefs();
+
+			string prefsFile = Path.Combine(Application.CommonAppDataPath, "Prefs.xml");
+			if (File.Exists(prefsFile))
+			{
+				XmlSerializer xml = new XmlSerializer(typeof(Prefs));
+				FileStream fs = new FileStream(prefsFile, FileMode.Open, FileAccess.Read);
+				prefs = (Prefs)xml.Deserialize(fs);
+				fs.Close();
+			}
+			else
+			{
+				FolderBrowserDialog fbd = new FolderBrowserDialog();
+				fbd.Description = "Select folder with texture data";
+				if (fbd.ShowDialog(this) != DialogResult.OK)
+				{
+					this.Close();
+					return;
+				}
+				else
+				{
+					prefs.DataPath = fbd.SelectedPath;
+					SavePrefs(prefs);
+				}
+			}
+			Editor = new ClusterEditor(prefs);
+
+			PlanView = new PlanRenderer(MapOverviewBox, Editor.TheWorld, Editor.ThePrefs);
+		}
     }
 }
