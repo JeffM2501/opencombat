408718bda623a526e029bbcc473e24de804c4d87
jeffm2501 <jeffm2501@4c235640-23d2-11df-bd34-830ff933dde4>
2012-08-24 00:01:35 +0000

    move avatar drawing to it's own lib to make the world drawing code easier to use in an editor.

---
+++ unit0/GameDrawing/ActorRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Game;
+
+namespace GameDrawing
+{
+    public class ActorRenderer : IDisposable
+    {
+        public GameState.BoundableActor Actor = null;
+
+        public ActorRenderer(GameState.BoundableActor actor)
+        {
+            Actor = actor;
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+
+        public virtual void Dispose()
+        {
+
+        }
+    }
+}
